0427acab8c759188bd1a7a2722ee6f24d0aabef3
Guillaume <yoguill85@gmail.com>
2023-03-01 20:52:51 +0100

    finition commit terminé

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -14,7 +14,7 @@ const WhatsNew = () => (
     initial="hidden"
     whileInView="show"
     viewport={{once:'false',amount: 0.25}}
-    className={`${styles.innerWidth} max-auto flex lg:flex-row flex-col gap-8`}
+    className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
 
     >
      
